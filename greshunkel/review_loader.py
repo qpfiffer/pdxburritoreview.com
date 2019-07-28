517a9b39003e7387b76df5b901c1cafbc3aa5ee6
Quinlan Pfiffer <qpfiffer@gmail.com>
2019-07-28 16:36:40 -0700

    Some progress

--- greshunkel/review_loader.py
+++ greshunkel/review_loader.py
@@ -1,4 +1,7 @@
 from yaml import safe_load
+import json, os
+
+REVIEW_DIR = "./built/data/"
 
 class ReviewLoader(object):
     def __init__(self, reviews_file):
@@ -6,9 +9,14 @@ class ReviewLoader(object):
         self.reviews = []
         self.regions = self.raw_reviews
 
+    def write(self):
+        os.makedirs(REVIEW_DIR, exist_ok=True)
+        with open("{}/reviews.json".format(REVIEW_DIR), "w") as reviews_file:
+            json.dump(self.raw_reviews, reviews_file)
+
     def process(self):
         for region in self.raw_reviews:
             for region_str, all_stores in region.items():
                 for store in all_stores:
-                    copy = dict(store)
-                    copy['region'] = region_str
+                    store['region'] = region_str
+        self.write()
